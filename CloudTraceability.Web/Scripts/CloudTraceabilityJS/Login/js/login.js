3a1969909ea94594aed5ebe737d1c194955a3bcc
gdnyfcuso <gdnyfcuso@163.com>
2018-03-26 22:01:21 +0800

    登录界面

---
+++ CloudTraceability.Web/Scripts/CloudTraceabilityJS/Login/js/login.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('.forgot-pass').click(function (event) {
+        $(".pr-wrap").toggleClass("show-pass-reset");
+    });
+    $('.pass-reset-submit').click(function (event) {
+        $(".pr-wrap").removeClass("show-pass-reset");
+    });
+});
